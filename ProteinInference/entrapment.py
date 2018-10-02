e513707a5608e86a87615a000f4348cf5ebd0e6a
Trent Hinkle <hinkle.trent@gene.com>
2018-10-02 11:22:50 -0700

    simplifying entrapment fdr calculation

--- ProteinInference/entrapment.py
+++ ProteinInference/entrapment.py
@@ -31,9 +31,8 @@ class GeneratePlot(Entrapment):
     This class requires the search being used to be from a benchmark dataset of KNOWN protein content.
     """
 
-    def __init__(self, data_class, entrapment_db, true_db, search_id, pdf=None, picked=None, qvr=None, pvr=None, other_database = None, search_database = None):
+    def __init__(self, data_class, true_db, search_id, pdf=None, picked=None, qvr=None, pvr=None, other_database = None, search_database = None, x=.1, y=.1):
         self.data_class = data_class
-        self.entrapment_db = entrapment_db
         self.true_db = true_db
         self.grouped_scored_data = data_class.grouped_scored_proteins
         self.search_id = search_id
@@ -61,11 +60,13 @@ class GeneratePlot(Entrapment):
         self.average_entrapment_fdr = None
         self.average_decoy_fdr = None
         self.pdf = pdf
+        self.x = x
+        self.y = y
 
         fdr1 = ProteinInference.fdrcalc.PureDecoyFdr(data_class=data_class, false_discovery_rate=0)
         fdr1.execute()
         self.decoy_fdr_calc_list = fdr1.fdr_list
-        efdr1 = ProteinInference.fdrcalc.EntrapFdr(data_class=data_class, entrapment_database=self.entrapment_db,
+        efdr1 = ProteinInference.fdrcalc.EntrapFdr(data_class=data_class,
                                                    true_database=self.true_db,other_database=self.other_database, false_discovery_rate=0)
         efdr1.execute()
         self.entrapment_fdr_calc_list = efdr1.fdr_list
@@ -82,38 +83,14 @@ class GeneratePlot(Entrapment):
             if '#' not in records.id:
                 true_proteins.append(records.id)
 
-        # search_handle = SeqIO.parse(self.search_database, 'fasta')
-        #
-        # search_proteins = []
-        # for records in search_handle:
-        #     search_proteins.append(records.id)
-        #
-        # decoy_search_proteins = [x for x in search_proteins if "#" in x]
 
-        entrapment_handle = SeqIO.parse(self.entrapment_db, 'fasta')
-
-        # Get the entrapment proteins minus the decoys...
-        entrapment_proteins = []
-        for records in entrapment_handle:
-            if '#' not in records.id:
-                entrapment_proteins.append(records.id)
 
         # Get the proteins from the search...
         protein_data = [x[0].identifier for x in self.grouped_scored_data]
 
-        # Add in proteins found in the search that arent decoys and arent already in entrapment proteins...
-        for prots in protein_data:
-            if '#' not in prots and prots not in entrapment_proteins:
-                entrapment_proteins.append(prots)
 
         print 'Number of Grouped Proteins = '+str(len(protein_data))
 
-
-        # This is with NO Decoys...
-        true_to_entrap_ratio = float(len(true_proteins)+1000)/float(len(entrapment_proteins))
-        # decoy_to_entrap_ratio = float(len(entrapment_proteins))/float(len(decoy_search_proteins))
-        print str(true_to_entrap_ratio)
-        # print str(decoy_to_entrap_ratio)
 
         false_true_positives = []
         decoys = []
@@ -139,6 +116,11 @@ class GeneratePlot(Entrapment):
         # self.decoy_fdr_list.append(decoy_fdr)
         # self.entrapment_fdr_list.append(entrapment_fdr)
 
+        # This is with NO Decoys...
+        true_to_entrap_ratio = float(len(true_proteins))/float(len(false_true_positives))
+        # decoy_to_entrap_ratio = float(len(entrapment_proteins))/float(len(decoy_search_proteins))
+        print "True Proteins Divided by False True Positives "+str(true_to_entrap_ratio)
+
         print 'Number of entraped proteins = '+str(len(false_true_positives))
         print 'Number of decoy proteins = '+str(len(decoys))
 
@@ -155,7 +137,7 @@ class GeneratePlot(Entrapment):
         # plt.show()
         # plt.close()
 
-        efdr05 = ProteinInference.fdrcalc.EntrapFdr(data_class=self.data_class, entrapment_database=self.entrapment_db,
+        efdr05 = ProteinInference.fdrcalc.EntrapFdr(data_class=self.data_class,
                                                     true_database=self.true_db,other_database=self.other_database,
                                                     false_discovery_rate=.05)
         efdr05.execute()
@@ -169,77 +151,77 @@ class GeneratePlot(Entrapment):
         plt.plot([0, 0.6666666666666666], '--')
         plt.plot([0, 1.5], '--')
         plt.plot(self.decoy_fdr_calc_list, self.entrapment_fdr_calc_list,'-o')
-        plt.xlim([0.00001, .5])
-        plt.ylim([0.00001, .5])
+        plt.xlim([0.00001, self.x])
+        plt.ylim([0.00001, self.y])
         plt.xlabel('Decoy FDR')
         plt.ylabel('Entrapment FDR')
-        plt.title(self.data_class.score_method + ' with '+str(len(efdr05.restricted_proteins))+' passing proteins' + '\n' + 'SearchID: ' + self.search_id + ', ' +self.pick + ', qvr = '+str(self.qvr)+ ', pvr = '+str(self.pvr))
+        plt.title(self.data_class.score_method + ' with '+str(len(efdr05.restricted_proteins))+' passing proteins' + ' Ratio:' + str(true_to_entrap_ratio)[:5]+ '\n' + 'SearchID: ' + self.search_id + ', ' +self.pick + ', qvr = '+str(self.qvr)+ ', pvr = '+str(self.pvr))
         if self.pdf:
             self.pdf.savefig()
         plt.show()
         plt.close()
 
-        full_vertical_distance_list = []
-        for j in range(len(self.decoy_fdr_calc_list)):
-            full_vertical_distance = self.entrapment_fdr_calc_list[j]-self.decoy_fdr_calc_list[j]
-            full_vertical_distance_list.append(abs(full_vertical_distance))
-
-        full_vertical_distance_mean = numpy.mean(full_vertical_distance_list)
-        self.full_vertical_distance_mean = full_vertical_distance_mean
-
-        full_perp_dist_list = []
-        p1 = numpy.array([0, 0])
-        p2 = numpy.array([1, 1])
-
-        for i in range(len(self.decoy_fdr_calc_list)):
-            p3 = numpy.array([self.decoy_fdr_calc_list[i], self.entrapment_fdr_calc_list[i]])
-
-            full_perp_distance = numpy.linalg.norm(numpy.cross(p2-p1, p1-p3))/numpy.linalg.norm(p2-p1)
-            full_perp_dist_list.append(full_perp_distance)
-
-        full_perp_dist_mean = numpy.mean(full_perp_dist_list)
-        self.full_perp_dist_mean = full_perp_dist_mean
-
-        vertical_distance_list = []
-        for j in range(len(self.decoy_fdr_calc_list)):
-            if self.decoy_fdr_calc_list[j]<.05:
-                vertical_distance = self.entrapment_fdr_calc_list[j] - self.decoy_fdr_calc_list[j]
-                vertical_distance_list.append(abs(vertical_distance))
-
-        vertical_distance_mean = numpy.mean(vertical_distance_list)
-        self.vertical_distance_mean = vertical_distance_mean
-
-        perp_dist_list = []
-        p1 = numpy.array([0, 0])
-        p2 = numpy.array([1, 1])
-
-        for i in range(len(self.decoy_fdr_calc_list)):
-            if self.decoy_fdr_calc_list[i]<.05:
-                p3 = numpy.array([self.decoy_fdr_calc_list[i], self.entrapment_fdr_calc_list[i]])
-
-                perp_distance = numpy.linalg.norm(numpy.cross(p2 - p1, p1 - p3)) / numpy.linalg.norm(p2 - p1)
-                perp_dist_list.append(perp_distance)
-
-        perp_dist_mean = numpy.mean(perp_dist_list)
-        self.perp_dist_mean = perp_dist_mean
-
-
-
-        xlist = [x for x in self.decoy_fdr_calc_list if x<.05]
-
-        data_array = numpy.array([self.entrapment_fdr_calc_list[x] for x in range(len(self.decoy_fdr_calc_list)) if self.decoy_fdr_calc_list[x]<.05])
-
-        test_array = numpy.array([x for x in xlist])
-
-        self.data_array = data_array
-        self.test_array = test_array
-
-
-        pr = scipy.stats.pearsonr(test_array,data_array)
-
-        pr = pr[0]
-
-        self.pr = pr
+        # full_vertical_distance_list = []
+        # for j in range(len(self.decoy_fdr_calc_list)):
+        #     full_vertical_distance = self.entrapment_fdr_calc_list[j]-self.decoy_fdr_calc_list[j]
+        #     full_vertical_distance_list.append(abs(full_vertical_distance))
+        #
+        # full_vertical_distance_mean = numpy.mean(full_vertical_distance_list)
+        # self.full_vertical_distance_mean = full_vertical_distance_mean
+        #
+        # full_perp_dist_list = []
+        # p1 = numpy.array([0, 0])
+        # p2 = numpy.array([1, 1])
+        #
+        # for i in range(len(self.decoy_fdr_calc_list)):
+        #     p3 = numpy.array([self.decoy_fdr_calc_list[i], self.entrapment_fdr_calc_list[i]])
+        #
+        #     full_perp_distance = numpy.linalg.norm(numpy.cross(p2-p1, p1-p3))/numpy.linalg.norm(p2-p1)
+        #     full_perp_dist_list.append(full_perp_distance)
+        #
+        # full_perp_dist_mean = numpy.mean(full_perp_dist_list)
+        # self.full_perp_dist_mean = full_perp_dist_mean
+        #
+        # vertical_distance_list = []
+        # for j in range(len(self.decoy_fdr_calc_list)):
+        #     if self.decoy_fdr_calc_list[j]<.05:
+        #         vertical_distance = self.entrapment_fdr_calc_list[j] - self.decoy_fdr_calc_list[j]
+        #         vertical_distance_list.append(abs(vertical_distance))
+        #
+        # vertical_distance_mean = numpy.mean(vertical_distance_list)
+        # self.vertical_distance_mean = vertical_distance_mean
+        #
+        # perp_dist_list = []
+        # p1 = numpy.array([0, 0])
+        # p2 = numpy.array([1, 1])
+        #
+        # for i in range(len(self.decoy_fdr_calc_list)):
+        #     if self.decoy_fdr_calc_list[i]<.05:
+        #         p3 = numpy.array([self.decoy_fdr_calc_list[i], self.entrapment_fdr_calc_list[i]])
+        #
+        #         perp_distance = numpy.linalg.norm(numpy.cross(p2 - p1, p1 - p3)) / numpy.linalg.norm(p2 - p1)
+        #         perp_dist_list.append(perp_distance)
+        #
+        # perp_dist_mean = numpy.mean(perp_dist_list)
+        # self.perp_dist_mean = perp_dist_mean
+        #
+        #
+        #
+        # xlist = [x for x in self.decoy_fdr_calc_list if x<.05]
+        #
+        # data_array = numpy.array([self.entrapment_fdr_calc_list[x] for x in range(len(self.decoy_fdr_calc_list)) if self.decoy_fdr_calc_list[x]<.05])
+        #
+        # test_array = numpy.array([x for x in xlist])
+        #
+        # self.data_array = data_array
+        # self.test_array = test_array
+        #
+        #
+        # pr = scipy.stats.pearsonr(test_array,data_array)
+        #
+        # pr = pr[0]
+        #
+        # self.pr = pr
         #
         # pearson_list = []
         # for k in range(len(data_array)):

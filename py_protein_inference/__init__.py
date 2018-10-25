6e19c58d172bdc01529e4ebfc3cb3e2f4a1c98de
Trent Hinkle <hinkle.trent@gene.com>
2018-10-25 14:25:49 -0700

    changing import names in some files

--- py_protein_inference/__init__.py
+++ py_protein_inference/__init__.py
@@ -6,13 +6,13 @@ Created on Fri Dec  1 11:07:11 2017
 @author: hinklet
 """
 
-from ProteinInference import reader
-from ProteinInference import scoring
-from ProteinInference import picker
-from ProteinInference import grouping
-from ProteinInference import fdrcalc
-from ProteinInference import export
-from ProteinInference import datastore
-from ProteinInference import benchmark
-from ProteinInference import physical
-from ProteinInference import entrapment
+from py_protein_inference import reader
+from py_protein_inference import scoring
+from py_protein_inference import picker
+from py_protein_inference import grouping
+from py_protein_inference import fdrcalc
+from py_protein_inference import export
+from py_protein_inference import datastore
+from py_protein_inference import benchmark
+from py_protein_inference import physical
+from py_protein_inference import entrapment

afd410b5d17ca263e54bc114bdeeddf231d6023a
Trent Hinkle <hinkle.trent@gene.com>
2019-09-26 14:57:45 -0700

    updating version from 0.1.0 to 0.2.0 to account for new features to read in custom scores from input, run additive scoring, and to apply different grouping types

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     pbr=True,
     scripts=glob.glob("scripts/*.py"),
     name='protein_inference',
-    version='0.1.0',
+    version='0.2.0',
     packages=['digest', 'protein_inference','scripts'],
     url='',
     license='',

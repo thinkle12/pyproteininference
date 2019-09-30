9fb03b1b98bb390ce6bdaa7fa7fc9f9d4468f8ac
Trent Hinkle <hinkle.trent@gene.com>
2019-09-30 15:49:57 -0700

    COMPPROT-2223: Adding Logging messages to the export module

--- protein_inference/export.py
+++ protein_inference/export.py
@@ -8,6 +8,8 @@ Created on Fri Dec  1 10:49:47 2017
 
 import os
 import csv
+from logging import getLogger
+
 
 class Export(object):
     """
@@ -21,36 +23,44 @@ class Export(object):
 
     def export_to_csv(self, directory, export_type = "q_value"):
 
+        logger = getLogger('protein_inference.export.Export.export_to_csv')
+
         data = self.data_class
         tag = data.parameter_file_object.tag
 
         if 'leads' in export_type:
             filename = '{}_leads_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_leads_restricted(filename_out=complete_filepath)
 
         if 'all' in export_type:
             filename = '{}_all_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_all_restricted(complete_filepath)
 
         if 'comma_sep' in export_type:
             filename = '{}_comma_sep_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_comma_sep_restricted(complete_filepath)
 
         if 'q_value_comma_sep' in export_type:
             filename = '{}_q_value_comma_sep_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_q_value_comma_sep(complete_filepath)
 
         if 'q_value' in export_type:
             filename = '{}_q_value_leads_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_q_value_leads(complete_filepath)
 
         if 'q_value_all' in export_type:
             filename = '{}_q_value_all_{}_{}.csv'.format(tag, data.short_score_method, data.score)
+            logger.info("Exporting Protein Inference Data to File: {}".format(filename))
             complete_filepath = os.path.join(directory, filename)
             self.csv_export_q_value_all(complete_filepath)
 

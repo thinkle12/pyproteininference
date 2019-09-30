1f381dfc537645ad35b687715063cfd226e7fe22
Trent Hinkle <hinkle.trent@gene.com>
2019-09-30 14:02:30 -0700

    Updated Tests to make BRAF have no unique peptides. Thus changing the Results generated from the various methods

--- tests/glpkinout/glpkout_test_parsimony.sol
+++ tests/glpkinout/glpkout_test_parsimony.sol
@@ -1,38 +1,37 @@
 Problem:    glpkin_test_parsimony
-Rows:       15
+Rows:       14
 Columns:    8 (8 integer, 8 binary)
-Non-zeros:  29
+Non-zeros:  28
 Status:     INTEGER OPTIMAL
-Objective:  z = 6 (MINimum)
+Objective:  z = 5 (MINimum)
 
    No.   Row name        Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 z                           6                             
+     1 z                           5                             
      2 c1                          1             1               
      3 c2                          1             1               
      4 c3                          1             1               
      5 c4                          1             1               
-     6 c5                          1             1               
-     7 c6                          3             1               
-     8 c7                          1             1               
+     6 c5                          2             1               
+     7 c6                          1             1               
+     8 c7                          2             1               
      9 c8                          1             1               
-    10 c9                          2             1               
+    10 c9                          1             1               
     11 c10                         1             1               
-    12 c11                         2             1               
+    12 c11                         1             1               
     13 c12                         1             1               
     14 c13                         1             1               
-    15 c14                         2             1               
 
    No. Column name       Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 y[4]         *              1             0             1 
-     2 y[1]         *              1             0             1 
-     3 y[7]         *              1             0             1 
-     4 y[0]         *              1             0             1 
-     5 y[5]         *              0             0             1 
+     1 y[6]         *              1             0             1 
+     2 y[0]         *              1             0             1 
+     3 y[7]         *              0             0             1 
+     4 y[1]         *              0             0             1 
+     5 y[2]         *              1             0             1 
      6 y[3]         *              1             0             1 
-     7 y[2]         *              1             0             1 
-     8 y[6]         *              0             0             1 
+     7 y[4]         *              1             0             1 
+     8 y[5]         *              0             0             1 
 
 Integer feasibility conditions:
 

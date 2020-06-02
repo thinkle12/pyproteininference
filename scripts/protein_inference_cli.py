57dbb3e484e2deabde1a19b363b2eb6242264184
Trent Hinkle <hinklet@gene.com>
2020-06-02 14:18:17 -0700

    Updating protein inference cli script to allow multiple new input types that include directories and lists of files. Either target/decoy separate or combined.

--- scripts/protein_inference_cli.py
+++ scripts/protein_inference_cli.py
@@ -11,16 +11,24 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Protein Inference')
-parser.add_argument("-t","--target", dest="target", required=True,
-                    help="Input target psm output from percolator", metavar="FILE")
-parser.add_argument("-d","--decoy", dest="decoy", required=True,
-                    help="Input decoy psm output from percolator", metavar="FILE")
+parser.add_argument("-t","--target", dest="target", required=False,
+                    help="Input target psm output from percolator. Can either input one file or a list of files", metavar="FILE", nargs='+', type=str)
+parser.add_argument("-d","--decoy", dest="decoy", required=False,
+                    help="Input decoy psm output from percolator. Can either input one file or a list of files", metavar="FILE", nargs='+', type=str)
+parser.add_argument("-f","--combined_files", dest="combined_files", required=False,
+                    help="Input combined psm output from percolator. This should contain Target and Decoy PSMS. Can either input one file or a list of files", metavar="FILE", nargs='+', type=str)
 parser.add_argument("-o","--output", dest="dir_name", required=False,
-                    help="protein_inference Result Directory to write to - Name of file will be determined by parameters selected and searchID", metavar="FILE")
+                    help="protein_inference Result Directory to write to - Name of file will be determined by parameters selected and parameter tag", metavar="DIR")
+parser.add_argument("-a","--target_directory", dest="target_directory", required=False,
+                    help="Directory that contains either .txt or .tsv input target psm data. Make sure the directory ONLY contains result files", metavar="DIR")
+parser.add_argument("-b","--decoy_directory", dest="decoy_directory", required=False,
+                    help="Directory that contains either .txt or .tsv input decoy psm data. Make sure the directory ONLY contains result files", metavar="DIR")
+parser.add_argument("-c","--combined_directory", dest="combined_directory", required=False,
+                    help="Directory that contains either .txt or .tsv input data with targets/decoys combined. Make sure the directory ONLY contains result files", metavar="DIR")
 parser.add_argument("-db","--database", dest="database", required=True,
-                    help="Provide the database used in the MS search", metavar="FILE")
-parser.add_argument("-ym","--yaml_params", dest="yaml_params", required=False,
-                    help="Provide a Protein Inference Yaml Parameter File... If none given, default parameters will be ran", metavar="FILE")
+                    help="Provide the fasta formatted database used in the MS search", metavar="FILE")
+parser.add_argument("-y","--yaml_params", dest="yaml_params", required=True,
+                    help="Provide a Protein Inference Yaml Parameter File", metavar="FILE")
 args = parser.parse_args()
 
 
@@ -28,6 +36,9 @@ pipeline = protein_inference.pipeline.ProteinInferencePipeline(parameter_file=ar
                                                                database_file=args.database,
                                                                target_files=args.target,
                                                                decoy_files=args.decoy,
-                                                               files=None,
+                                                               combined_files=args.combined_files,
+                                                               target_directory=args.target_directory,
+                                                               decoy_directory=args.decoy_directory,
+                                                               combined_directory=args.combined_directory,
                                                                output_directory=args.dir_name)
 pipeline.execute()

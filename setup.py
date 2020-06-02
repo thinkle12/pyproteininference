d2320be43970f52161843492eeb07a02aa017184
Trent Hinkle <hinklet@gene.com>
2020-06-02 14:10:47 -0700

    Upping version to 0.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     pbr=True,
     scripts=glob.glob("scripts/*.py"),
     name='protein_inference',
-    version='0.3.2',
+    version='0.3.3',
     url='',
     license='',
     author='hinklet',

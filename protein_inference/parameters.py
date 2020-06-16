06d1d2a9b1af4a34bf0a5e0bd52635aef07459f2
Trent Hinkle <hinklet@gene.com>
2020-06-15 21:07:19 -0700

    Adding parameter instance methods to override q/pep/custom restriction variables if it is determined that the value was defined in the params but the score value does not exist in the input

--- protein_inference/parameters.py
+++ protein_inference/parameters.py
@@ -299,3 +299,31 @@ class ProteinInferenceParameter(object):
     def _override_inference_none(self):
         if self.inference_type in ["None", "none", None]:
             self.inference_type="none"
+
+    def override_q_restrict(self, data_class):
+        data_has_q = data_class.input_has_q()
+        if data_has_q:
+            pass
+        else:
+            if self.restrict_q:
+                self.logger.warning("No Q values found in the input data, overriding parameters to not filter on Q value")
+                self.restrict_q = None
+
+    def override_pep_restrict(self, data_class):
+        data_has_pep = data_class.input_has_pep()
+        if data_has_pep:
+            pass
+        else:
+            if self.restrict_pep:
+                self.logger.warning("No Pep values found in the input data, overriding parameters to not filter on Pep value")
+                self.restrict_pep = None
+
+    def override_custom_restrict(self, data_class):
+        data_has_custom = data_class.input_has_custom()
+        if data_has_custom:
+            pass
+        else:
+            if self.restrict_custom:
+                self.logger.warning("No Custom values found in the input data, overriding parameters to not filter on Custom value")
+                self.restrict_custom = None
+

5db03e782aaf214df4136b22575cdee8fa6b8208
Trent Hinkle <hinklet@gene.com>
2020-07-15 23:13:44 -0700

    Updating tests to work with lp solver as None type as opposed to none strings

--- tests/test_002_inclusion_pipeline.py
+++ tests/test_002_inclusion_pipeline.py
@@ -99,7 +99,7 @@ class TestLoadInclusionWorkflow(TestCase):
         self.assertEqual(
             protein_inference_parameters.max_identifiers_peptide_centric, 5
         )
-        self.assertEqual(protein_inference_parameters.lp_solver, "None")
+        self.assertEqual(protein_inference_parameters.lp_solver, None)
 
         ### STEP 2: Start with running an In Silico Digestion ###
         ### STEP 2: Start with running an In Silico Digestion ###

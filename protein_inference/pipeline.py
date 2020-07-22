dde3303de34c2f50e32f98495c7995f584559019
Trent Hinkle <hinklet@gene.com>
2020-07-22 14:44:23 -0700

    Adding a new class method to pipeline that validates bool input

--- protein_inference/pipeline.py
+++ protein_inference/pipeline.py
@@ -356,3 +356,14 @@ class ProteinInferencePipeline(object):
             self.output_directory = os.getcwd()
         else:
             pass
+
+    @classmethod
+    def str2bool(self, v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')

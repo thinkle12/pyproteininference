0ef8b039e4d3817b5078af1abf874c187dba4d8d
Trent Hinkle <hinklet@gene.com>
2022-06-17 14:34:53 -0700

    Applied changes to README, setup.cfg, and setup.py for deployment to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from setuptools import setup
 import glob
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     setup_requires=["pbr>=1.8", "setuptools>=17.1"],
     pbr=True,
     scripts=glob.glob("scripts/*.py"),
     name="pyproteininference",
-    version="0.9.8",
-    url="",
-    license="",
-    author="hinklet",
+    version="1.0.0",
+    url="https://github.com/thinkle12/pyproteininference",
+    license="Apache-2",
+    author="Trent Hinkle",
     author_email="hinklet@gene.com",
-    description="Python Package for running custom Protein Inference on tab-formatted tandem MS/MS search results",
+    description="Python Package for running protein inference algorithms on tab-formatted tandem MS/MS search results.",
+    keywords = ['protein inference', 'proteomics', 'mass spectrometry'],
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Bio-Informatics',
+      'License :: OSI Approved :: Apache Software License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
